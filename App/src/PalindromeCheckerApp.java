42a3f3cd0e5f51f030a394eb3aeef4acf1ff8173
AakritiAgarwal001 <aa0081@srmist.edu.in>
2026-02-24 16:48:05 +0530

    UC9: Recursive Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,24 @@
 import java.util.*;
 public class PalindromeCheckerApp {
-    public static void main(String[] args)
-    {
-        Scanner scnr=new Scanner(System.in);
-        System.out.print("Input text:");
-        String str=scnr.nextLine();
-        LinkedList<Character> list = new LinkedList<>();
-        for (char c : str.toCharArray()) {
-            list.addLast(c);
+        public static boolean isPalindrome(String str, int start, int end) {
+            if (start >= end) {
+            return true;
         }
-        boolean isPalin=true;
-        while (list.size() > 1) {
-            if (!list.removeFirst().equals(list.removeLast())) {
-                isPalin = false;
-                break;
-            }
+            if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-        System.out.print("\nIs it a palindrome? :"+isPalin);
+            return isPalindrome(str, start + 1, end - 1);
+    }
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        input = input.replaceAll("\\s+", "").toLowerCase();
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+        if (result) {
+            System.out.println("Result: The string is a PALINDROME.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
     }
 }
